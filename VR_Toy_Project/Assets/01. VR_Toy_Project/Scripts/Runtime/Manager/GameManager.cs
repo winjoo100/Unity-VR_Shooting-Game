18d23e7f2ac661891101d3cf00852cd884b4b5a3
onasheep <94965545+onasheep@users.noreply.github.com>
2023-10-13 13:10:07 +0900

    TEST : UI 상호작용 테스트

--- VR_Toy_Project/Assets/01. VR_Toy_Project/Scripts/Runtime/Manager/GameManager.cs
+++ VR_Toy_Project/Assets/01. VR_Toy_Project/Scripts/Runtime/Manager/GameManager.cs
@@ -2,20 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : GSingleton<GameManager>
+public class GameManager : MonoBehaviour
 {
     #region Variables
-    private bool isGame = default;
+    // 게임 사이클 변수
+    private bool isStart = default;
+    private bool isEnd = default;
+    // 게임 사이클 변수
     #endregion
 
     // Start is called before the first frame update
-    public override void Start()
+    void Start()
     {
 
     }
 
     // Update is called once per frame
-    public override void Update()
+    void Update()
     {
         
     }
