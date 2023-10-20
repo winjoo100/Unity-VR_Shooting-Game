6c535554e1d68d9ff7560f4fe0afac938190da41
BaekSeungJoo <lueun59@gmail.com>
2023-10-20 10:44:57 +0900

    Feat : AddMap
    터레인 자갈로 변경, 스카이박스 배치, 지구 재배치, 조명 재배치하였음
    몬스터 스포너 수정하였음.

--- VR_Toy_Project/Assets/_PJK/Scripts/Boss/MonsterSpawner.cs
+++ VR_Toy_Project/Assets/_PJK/Scripts/Boss/MonsterSpawner.cs
@@ -14,6 +14,9 @@ public class MonsterSpawner : MonoBehaviour
     public GameObject spawnPoint01;
     public GameObject spawnPoint02;
 
+    // BSJ _ 테스트 용 시간 단축 
+    public float testTimeReduce;
+
     private float spawnTime = 5f;
     
 
@@ -35,7 +38,7 @@ public class MonsterSpawner : MonoBehaviour
     private void CreateMonster()
     {
 
-            if (BossManager.instance.currentTime < 300f)
+            if (BossManager.instance.currentTime < 300f * testTimeReduce)
             {
                 for (int i = 0; i < 3; i++)
                 {
@@ -43,7 +46,7 @@ public class MonsterSpawner : MonoBehaviour
 
                 }
             }
-            else if (300f < BossManager.instance.currentTime && BossManager.instance.currentTime < 600f)
+            else if (300f * testTimeReduce < BossManager.instance.currentTime && BossManager.instance.currentTime < 600f * testTimeReduce)
             {
                 for (int i = 0; i < 4; i++)
                 {
@@ -51,7 +54,7 @@ public class MonsterSpawner : MonoBehaviour
 
                 }
             }
-            else if (600f < BossManager.instance.currentTime && BossManager.instance.currentTime < 900f)
+            else if (600f * testTimeReduce < BossManager.instance.currentTime && BossManager.instance.currentTime < 900f * testTimeReduce)
             {
                 for (int i = 0; i < 5; i++)
                 {
@@ -76,14 +79,14 @@ public class MonsterSpawner : MonoBehaviour
 
     void SecondWave()
     {
-        int spawnx = Random.Range(-500, 500); 
-        GameObject Mon2 = Instantiate(Monsterlv2, _monsterSpawner.transform);
+        int spawnx = Random.Range((int)spawnPoint01.transform.position.x, (int)spawnPoint02.transform.position.x); 
+        GameObject Mon2 = Instantiate(Monsterlv2, _monsterSpawner.transform.position, Quaternion.identity);
         Mon2.transform.position = new Vector3(spawnx, _monsterSpawner.transform.position.y, _monsterSpawner.transform.position.z);
     }
     void ThirdWave()
     {
-        int spawnx = Random.Range(-500, 500);
-        GameObject Mon3 = Instantiate(Monsterlv3, _monsterSpawner.transform);
+        int spawnx = Random.Range((int)spawnPoint01.transform.position.x, (int)spawnPoint02.transform.position.x);
+        GameObject Mon3 = Instantiate(Monsterlv3, _monsterSpawner.transform.position, Quaternion.identity);
         Mon3.transform.position = new Vector3(spawnx, _monsterSpawner.transform.position.y, _monsterSpawner.transform.position.z);
     }
 
