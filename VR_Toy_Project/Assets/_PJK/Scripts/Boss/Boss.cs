5588fd9dd4754e66d8c32cff788c889e403f74fc
DESKTOP-UIL7QR9\KGA <pjk2155@naver.com>
2023-10-23 13:26:32 +0900

    Feat.BossDeadEffect and BossAttackEffect
    보스 사망 효과와
    보스 공격 들의 죽음 효과 추가

--- VR_Toy_Project/Assets/_PJK/Scripts/Boss/Boss.cs
+++ VR_Toy_Project/Assets/_PJK/Scripts/Boss/Boss.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using UnityEngine;
 
 public class Boss : MonoBehaviour, IDamageable
-{    
+{
     public GameObject boss = default;
     public GameObject player = default;
     public GameObject[] Turret = default;
+    public GameObject bossDiePrefab = default;
+    private GameObject bossDie = default;
     // 터렛을 타겟중인지 체크
     public bool isFindTurret = false;
     // 터렛을 공격중인지 체크
@@ -78,40 +80,12 @@ public class Boss : MonoBehaviour, IDamageable
 
     private void Update()
     {
-        // 정근정근아 작동되는 코드를 넣어라
-        //if (m.isFindTurret == true)
-        //{
-        //    if (m.isAttackTurret == true)
-        //    {
-        //        //AttackTurret();
-        //        rb.velocity = Vector3.zero;
-        //    }
-        //}
 
-        //if (bm.Weaknesstime > 10 || weakActiveCount == 0)
-        //{
-        //    // 기존 약점 비활성화
-        //    //OffWeakPoint();
 
-        //    // 약점 활성화
-        //    //OnWeakPoint();
-        //    bm.Weaknesstime = 0;
-        //}
 
-        if (CurHP <= 0)
-        {
-
-            Death();
-        }
 
 
     }
-
-    public void FindTurret()
-    {
-
-    }
-
 
     private void AttackedWeakPoint()
     {
@@ -119,48 +93,27 @@ public class Boss : MonoBehaviour, IDamageable
     }
 
 
-    private void Death()
-    {
-        
-        isDead = true;
-        //TODO : 게임종료 승리
 
-    }
-
-    //void OnWeakPoint()
-    //{
-    //    for (int i = 0; i < 3; i++)
-    //    {
-    //        int onweak = Random.Range(0, 8);
-
-    //        weakpoints[onweak].SetActive(true);
-
-    //        if (i == 0)
-    //        {
-    //            weakpoints[onweak].transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
-    //        }
-
-    //        weakActiveCount = 3;
-    //    }
-    //}
-
-           // weakpoints[i].SetActive(false);
-       // }
-      //  weakActiveCount = 0;
- //   }
 
 
     public void OnDamage(int damage)
     {
         CurHP -= damage;
         CalculateHp();
+
+        if (CurHP <= 0 || Input.GetKeyDown(KeyCode.X))
+        {
+            bossDie = Instantiate(bossDiePrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+
+        }
     }
 
     // ! 보스 체력 퍼센트 비례로 돈을 얻는 함수
     private void CalculateHp()
-    {        
+    {
         int rateHp = (int)(MaxHp * 0.1f);
-        
+
         if (CurHP <= lastGoldHP - rateHp)
         {
             GameManager.Instance.GetGold_Boss();
