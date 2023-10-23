c675e8677f3bd5d769e227011fe2592763fd119d
BaekSeungJoo <lueun59@gmail.com>
2023-10-23 17:22:51 +0900

    Feat : SoundManager
    사운드 매니저 추가하였음.

---
+++ VR_Toy_Project/Assets/_BSJ/Scripts/TimeScaleTest.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class TimeScaleTest : MonoBehaviour
+{
+    public void Start()
+    {
+        Time.timeScale = 0f;
+    }
+}
